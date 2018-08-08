adf20d9ed05057f35e203c5dbf8c6d3af1bdaa72
Jonathan CHIRK <jchirk@e3r8p17.42.fr>
2018-08-08 20:13:08 +0200

    J05-3

--- j05/ex05/ft_strstr.c
+++ j05/ex05/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: jchirk <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/05 21:02:42 by jchirk            #+#    #+#             */
-/*   Updated: 2018/08/08 16:32:38 by jchirk           ###   ########.fr       */
+/*   Updated: 2018/08/08 19:26:05 by jchirk           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,16 @@ char		*ft_strstr(char *str, char *to_find)
 	int		j;
 
 	i = 0;
-	j = 0;
 	if (to_find[0] == '\0')
 		return (str);
 	while (str[i] != '\0')
 	{
-		while (to_find[j] == str[i] && to_find[j] != '\0' && str[i] != '\0')
+		j = 0;
+		while (to_find[j] == str[i + j])
 		{
-			i++;
 			j++;
-			if (to_find[j] == '\0' && to_find[j - 1] == str[i - 1])
-				return ((str + i - j));
+			if (to_find[j] == '\0')
+				return (str + i);
 		}
 		i++;
 	}
